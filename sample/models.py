dfbd94d25dd21663a6c99b5cb04d6537e63b2914
Kan Takamasa <taka.96kc@gmail.com>
2018-12-06 00:13:12 +0900

    add : sampleテーブル作成

--- sample/models.py
+++ sample/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='カテゴリー名')
+    is_deleted = models.IntegerField(default='0', verbose_name='削除フラグ')
+
+class Blogs(models.Model):
+    title = models.CharField(max_length=100, verbose_name='タイトル')
+    content = models.TextField(verbose_name='内容')
+    is_deleted = models.IntegerField(default='0', verbose_name='削除フラグ')
+    category = models.IntegerField(default='1', verbose_name='カテゴリーID')
+    created_at = models.DateTimeField(default='current_date', verbose_name='作成日')
+    updated_at = models.DateTimeField(default='current_date', verbose_name='更新日')
