6c5cde971b0b9a3879f15eac10372a714259404f
Kan Takamasa <taka.96kc@gmail.com>
2019-01-12 02:01:00 +0900

    add : 共通のテンプレートに変更とダイアログボックス追加

--- static/js/base.js
+++ static/js/base.js
@@ -2,3 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
 });
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+    $('.modal-trigger').leanModal();
+});
